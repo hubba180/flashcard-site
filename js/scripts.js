d5e159e7e93a2ff2c76b515998b2e64ad07d1f09
Kevin Davis <thekidnamedkd@gmail.com>
2020-06-02 17:05:33 -0700

    establish javascript click functions to toggle question and definitions. style each item to feel like a flash card and highlight.
    Co-authored-by: Kyle Hubbard <kyle.james.hubbard@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,172 @@
 $(document).ready(function() {
-  $("li").click(function() {
-    $("").toggle():
+  $("li#java-q").click(function() {
+    $("#java-q").toggle();
+    $("#java-def").toggle();
+  })
+  $("li#java-def").click(function() {
+    $("#java-q").toggle();
+    $("#java-def").toggle();
+  })
+
+  $("li#opera-q").click(function() {
+    $("#opera-q").toggle();
+    $("#opera-def").toggle();
+  })
+  $("li#opera-def").click(function() {
+    $("#opera-q").toggle();
+    $("#opera-def").toggle();
+  })
+
+  $("li#vari-q").click(function() {
+    $("#vari-q").toggle();
+    $("#vari-def").toggle();
+  })
+  $("li#vari-def").click(function() {
+    $("#vari-q").toggle();
+    $("#vari-def").toggle();
+  })
+
+  $("li#namecon-q").click(function() {
+    $("#namecon-def").toggle();
+    $("#namecon-q").toggle();
+  })
+  $("li#namecon-def").click(function() {
+    $("#namecon-q").toggle();
+    $("#namecon-def").toggle();
+  })
+
+  $("li#func-q").click(function() {
+    $("#func-q").toggle();
+    $("#func-def").toggle();
+  })
+  $("li#func-def").click(function() {
+    $("#func-q").toggle();
+    $("#func-def").toggle();
+  })
+
+  $("li#meth-q").click(function() {
+    $("#meth-q").toggle();
+    $("#meth-def").toggle();
+  })
+  $("li#meth-def").click(function() {
+    $("#meth-q").toggle();
+    $("#meth-def").toggle();
+  })
+
+  $("li#argu-q").click(function() {
+    $("#argu-q").toggle();
+    $("#argu-def").toggle();
+  })
+  $("li#argu-def").click(function() {
+    $("#argu-q").toggle();
+    $("#argu-def").toggle();
+  })
+
+  $("li#para-q").click(function() {
+    $("#para-q").toggle();
+    $("#para-def").toggle();
+  })
+  $("li#para-def").click(function() {
+    $("#para-q").toggle();
+    $("#para-def").toggle();
+  })
+
+  $("li#return-q").click(function() {
+    $("#return-q").toggle();
+    $("#return-def").toggle();
+  })
+  $("li#return-def").click(function() {
+    $("#return-q").toggle();
+    $("#return-def").toggle();
+  })
+
+  $("li#chain-q").click(function() {
+    $("#chain-q").toggle();
+    $("#chain-def").toggle();
+  })
+  $("li#chain-def").click(function() {
+    $("#chain-q").toggle();
+    $("#chain-def").toggle();
+  })
+
+  $("li#strings-q").click(function() {
+    $("#strings-q").toggle();
+    $("#strings-def").toggle();
+  })
+  $("li#strings-def").click(function() {
+    $("#strings-q").toggle();
+    $("#strings-def").toggle();
+  })
+
+  $("li#bool-q").click(function() {
+    $("#bool-q").toggle();
+    $("#bool-def").toggle();
+  })
+  $("li#bool-def").click(function() {
+    $("#bool-q").toggle();
+    $("#bool-def").toggle();
+  })
+
+  $("li#undef-q").click(function() {
+    $("#undef-q").toggle();
+    $("#undef-def").toggle();
+  })
+  $("li#undef-def").click(function() {
+    $("#undef-q").toggle();
+    $("#undef-def").toggle();
+  })
+
+  $("li#nan-q").click(function() {
+    $("#nan-q").toggle();
+    $("#nan-def").toggle();
+  })
+  $("li#nan-def").click(function() {
+    $("#nan-q").toggle();
+    $("#nan-def").toggle();
+  })
+
+  $("li#null-q").click(function() {
+    $("#null-q").toggle();
+    $("#null-def").toggle();
+  })
+  $("li#null-def").click(function() {
+    $("#null-q").toggle();
+    $("#null-def").toggle();
+  })
+
+  $("li#alert-q").click(function() {
+    $("#alert-q").toggle();
+    $("#alert-def").toggle();
+  })
+  $("li#alert-def").click(function() {
+    $("#alert-q").toggle();
+    $("#alert-def").toggle();
+  })
+
+  $("li#comm-q").click(function() {
+    $("#comm-q").toggle();
+    $("#comm-def").toggle();
+  })
+  $("li#comm-def").click(function() {
+    $("#comm-q").toggle();
+    $("#comm-def").toggle();
+  })
+
+  $("li#jquery-q").click(function() {
+    $("#jquery-q").toggle();
+    $("#jquery-def").toggle();
+  })
+  $("li#jquery-def").click(function() {
+    $("#jquery-q").toggle();
+    $("#jquery-def").toggle();
+  })
+
+  $("li#attrib-q").click(function() {
+    $("#atrrib-q").toggle();
+    $("#attrib-def").toggle();
+  })
+  $("li#attrib-def").click(function() {
+    $("#attrib-q").toggle();
+    $("#attrib-def").toggle();
   })
 });
